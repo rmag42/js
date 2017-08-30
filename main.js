e6018113c733df446394cc55c00075e5dabbde98
unknown <rmag42@gmail.com>
2017-08-30 14:57:13 -0400

    created game logic

---
+++ main.js
@@ -0,0 +1,33 @@
+console.log("Up and running!");
+
+var cards = ['queen', 'queen', 'king', 'king'];
+
+var cardsInPlay = [];
+
+var checkForMatch = function(){
+if (cardsInPlay.length === 2){
+if (cardsInPlay[0] === cardsInPlay[1]){
+	console.log("You found a match!");
+}
+else {
+	console.log("Sorry, try again.");
+}
+}
+};
+
+var flipCard = function (cardId){
+console.log ("User flipped " + cards[cardId]);
+cardsInPlay.push (cards[cardId]);
+checkForMatch();
+};
+
+flipCard(0);
+flipCard(2);
+
+
+
+
+
+
+
+
